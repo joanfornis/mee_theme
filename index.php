958d9acd552b08054220f2eb6e7c7ff0e382a4d8
joan.fornis <joanfornis@gmail.com>
2022-05-02 10:46:37 +0200

    menu, cards para blog...

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php get_header(); ?>
-<link rel="stylesheet" href="/styles/blog-list.css">
+<link rel="stylesheet" href="/wp-content/themes/mee-theme/styles/blog-list.css">
 <div class="content-wrapper container">
     <main id="content" class="content" role="main">
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        <?php get_template_part( 'entry' ); ?>
-        <?php comments_template(); ?>
-        <?php endwhile; endif; ?>
-        <?php get_template_part( 'nav', 'below' ); ?>
+        <section class="blog-list row">
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <?php get_template_part( 'entry' ); ?>
+            <?php comments_template(); ?>
+            <?php endwhile; endif; ?>
+            <?php get_template_part( 'nav', 'below' ); ?>
+        </section>
     </main>
     <?php get_sidebar(); ?>
     <?php get_footer(); ?>
